9ffdd7e6c6f8195a32c20d058f01905513d66183
raphael allard <raphael.s.allard@gmail.com>
2021-07-13 09:51:17 +0200

    logout is now available

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :current_user
+
   def sign_in_user(user)
     user.expire_token!
     session[:email] = user.email
   end
   
   def current_user
-    User.find_by(email: session[:email])
+    @current_user = User.find_by(email: session[:email])
   end
 end
